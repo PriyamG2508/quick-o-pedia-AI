fcdc33c27a3a213df1f5cf10ded568b382c775fd
Priyam Gupta <priyamgupta2508@gmail.com>
2025-08-31 17:57:28 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,8 +19,7 @@ app = FastAPI(
 # CORS configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://quick-o-pedia-ai-gr4p5ky7s-priyam-guptas-projects-0d4ac92d.vercel.app",  # Your actual Vercel URL
-        "https://*.vercel.app",],  
+    allow_origins=["*"],  
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
