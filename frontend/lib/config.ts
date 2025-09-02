57a8d42f90b56e7f6dfbfd4b30c0ed7a04baf823
PriyamG2508 <priyamgupta2508@gmail.com>
2025-09-02 15:25:57 +0530

    change backend url

--- frontend/lib/config.ts
+++ frontend/lib/config.ts
@@ -5,11 +5,7 @@ console.log('Environment variable NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC
 
 export const config = {
   // Backend API URL - update this to match your backend server
-<<<<<<< HEAD
-  apiUrl: process.env.NEXT_PUBLIC_API_URL|| 'http://localhost:8000',
-=======
   apiUrl: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
->>>>>>> 7a2c3284b23b5543682ade08beb87cc167373353
   
   // App settings
   appName: 'Quick o Pedia',
